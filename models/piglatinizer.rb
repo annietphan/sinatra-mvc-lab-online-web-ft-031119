baa04b50fafb827208aca3fd50dd7edf470ddb19
github username <github email address>
2019-04-25 12:56:22 -0700

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+
+  def translate_word(text)
+    if /^[aeiou]/i.match(text)
+      "#{text}way"
+    else
+      parts = text.split(/([aeiou"].*)/)
+      "#{parts[1]}#{parts[0]}ay"
+    end
+  end
+
+  def piglatinize(text)
+    words = text.split(" ")
+    words.map do |word|
+      translate_word(word)
+    end.join(" ")
+  end
+
+end
